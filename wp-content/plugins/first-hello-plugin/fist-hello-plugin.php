084ad771e77cfcff3b6a6b7801e426f51b784fe2
behailu <angbehailu@gmail.com>
2025-02-19 09:07:14 +0300

    admin-notices and custom admin-dashboad-widget

--- wp-content/plugins/first-hello-plugin/fist-hello-plugin.php
+++ wp-content/plugins/first-hello-plugin/fist-hello-plugin.php
@@ -6,4 +6,29 @@ Version: 1.0.0
 Author: Behailu Mesganaw
 */
 
-?>
+// Add Admin Notice
+add_action('admin_notices','fhw_display_warning_message');
+function fhw_display_success_message(){
+    echo '<div class=" notice notice-success is-dismissible"><p>Hello, It is a success message from First Hello Plugin</p></div>';
+}
+
+function fhw_display_error_message(){
+    echo '<div class=" notice notice-error is-dismissible"><p>Hello, It is a error message from First Hello Plugin</p></div>';
+}
+
+function fhw_display_info_message(){
+    echo '<div class=" notice notice-info is-dismissible"><p>Hello, It is a information message from First Hello Plugin</p></div>';
+}
+
+function fhw_display_warning_message(){
+    echo '<div class=" notice notice-warning is-dismissible"><p>Hello, It is a warning message from First Hello Plugin</p></div>';
+}
+
+// Admin Dashboard Widget
+add_action('wp_dashboard_setup', 'fhw_dashboard_widget');
+function fhw_dashboard_widget() {
+    wp_add_dashboard_widget('fhw_hello_world', 'FHW, Hello World', 'fhw_custom_dashboard_widget');
+}
+function fhw_custom_dashboard_widget() {
+    echo 'This is a custom dashboard widget';
+}
