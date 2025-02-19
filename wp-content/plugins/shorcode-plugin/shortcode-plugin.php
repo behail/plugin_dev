ad02e60c5759a0c1fca5a4b3d97d8cab06a8c6b1
behailu <angbehailu@gmail.com>
2025-02-19 09:49:25 +0300

    parameterized shortcode

--- wp-content/plugins/shorcode-plugin/shortcode-plugin.php
+++ wp-content/plugins/shorcode-plugin/shortcode-plugin.php
@@ -10,3 +10,14 @@ add_shortcode('message', 'scp_show_static_message');
 function scp_show_static_message(){
     return "<p style='color:red; font-size:20px; font-weight:bold;'>Hello, It is a shortcode from Shortcode Plugin</p>";
 }
+
+// parameterized shortcode
+add_shortcode('message2', 'scp_show_static_message2');
+function scp_show_static_message2($attributes) {
+    $attributes = shortcode_atts(array(
+        'name' => 'Behailu Mesganaw',
+        'email' => 'behailu.mesganaw@gmail.com',
+    ), $attributes,'message2' );
+
+    return "<h3>Name: {$attributes['name']} -  Email: {$attributes['email']}</h3>";
+}
