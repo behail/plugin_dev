1454b5c488e9bcb1ddc10ad21556e0f9b7374a72
behailu <angbehailu@gmail.com>
2025-02-20 16:30:34 +0300

    wp-login page custumizer plugin start

---
+++ wp-content/plugins/bm-wp-login-customizer/bm-wp-login-customizer.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: BM WP Login Customizer
+ * Description: A plugin to customize the WP login page logo, Text Color, Background Color
+ * Version: 1.0.0
+ * Author: Behailu Mesganaw
+ * Author URI: https://github.com/behail
+ */
+
+ if(!defined('ABSPATH')) exit;
