660fb44e9b741905c01d6d1facebb35f35c96afe
behailu <angbehailu@gmail.com>
2025-02-20 17:47:16 +0300

    employee-crud plugin starter

---
+++ wp-content/plugins/bm-crud-employee-plugin/bm-crud-employee-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: BM CRUD Employee Plugin
+Description: A plugin to perform CRUD operation on employee table. It also create dynamic wordpress page and it will have a shortcode.
+Version: 1.0.0
+Author: Behailu Mesganaw
+*/
+
+if(!defined('ABSPATH')) exit;
+
