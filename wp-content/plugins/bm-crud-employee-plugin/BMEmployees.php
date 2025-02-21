ce0149946620723fca3c3609ec50351d413ffe27
behailu <angbehailu@gmail.com>
2025-02-21 09:14:02 +0300

    class based DB creation and drop on activation and diactivation

--- wp-content/plugins/bm-crud-employee-plugin/BMEmployees.php
+++ wp-content/plugins/bm-crud-employee-plugin/BMEmployees.php
@@ -2,10 +2,39 @@
 
 class BMEmployees {
 
-    private $wbdb;
+    private $wpdb;
+    private $table_prefix;  
     private $table_name;
     public function __construct(){
         global $wpdb;
-        $this->wbdb = $wpdb;
+        $this->wpdb = $wpdb;
+        $this->table_prefix = $this->wpdb->prefix;
+        $this->table_name = $this->table_prefix . 'bm_employees';
+    }
+
+    // Create DB table
+    public function createEmployeeTable(){ 
+
+        $collate = $this->wpdb->get_charset_collate();
+
+        $createCommand = "
+            CREATE TABLE `".$this->table_name."` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `name` varchar(50) NOT NULL,
+            `email` varchar(50) DEFAULT NULL,
+            `designation` varchar(50) DEFAULT NULL,
+            PRIMARY KEY (`id`)
+            ) ".$collate."
+            ";
+        require_once(ABSPATH . '/wp-admin/includes/upgrade.php');
+        dbDelta($createCommand);
+
+    }
+
+    // Drop DB table
+    public function dropEmployeeTable(){
+       $delete_command = "DROP TABLE IF EXISTS {$this->table_name}";
+       
+       $this->wpdb->query($delete_command);
     }
 }
--- wp-content/plugins/bm-crud-employee-plugin/bm-crud-employee-plugin.php
+++ wp-content/plugins/bm-crud-employee-plugin/bm-crud-employee-plugin.php
@@ -8,3 +8,17 @@ Author: Behailu Mesganaw
 
 if(!defined('ABSPATH')) exit;
 
+define('BMCP_DIR_PATH', plugin_dir_path( __FILE__ ));
+define('BMCP_DIR_URL', plugin_dir_url( __FILE__ ));
+
+include_once BMCP_DIR_PATH .'BMEmployees.php';
+
+// Create Class Object
+$bmEmployeesObj = new BMEmployees;
+
+// Create DB table
+register_activation_hook(__FILE__, [$bmEmployeesObj, 'createEmployeeTable']);
+
+
+// Drop DB table
+register_deactivation_hook(__FILE__, [$bmEmployeesObj, 'dropEmployeeTable']);
